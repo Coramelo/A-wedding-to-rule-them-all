a2527ba6456e4b8f25ad9637ccae0e19dc1f6140
Coramelo <alejandro_cora@hotmail.com>
2020-11-15 21:21:01 +0100

    Final code

--- js/scripts.js
+++ js/scripts.js
@@ -212,20 +212,20 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Stop a moment for a minute please!</strong> We are saving your details.'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyosE-bNLhXoDguEJiCRKJkYhKBG1UxGoxRKrWmOaWxvas2fxd5/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
                         $('#alert-wrapper').html(alert_markup('danger', data.message));
                     } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
+                        $('#alert-wrapper').html('Ha hecho su cosa');
+                        //$('#rsvp-modal').modal('show');
                     }
                 })
                 .fail(function (data) {
